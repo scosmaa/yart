85434a12b168cf586485835b6115cbfdd69fbf7f
scosmaa <scosmaa@gmail.com>
2019-01-11 14:07:40 +0100

    pagination draft

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -9,15 +9,35 @@ import './Table.scss'
 export default class Table extends Component {
     constructor(props) {
       super(props)
-    
+        let data = this._makeInternalId(props.data)
+
       this.state = {
-         originalData: this._makeInternalId(props.data),
-         data: this._makeInternalId(props.data),
-         columns: this._makeInternalId(props.columns),
-         sortField: props.sortField,
-         sortDirection: props.sortDirection,
+        originalData: this._makeInternalId(props.data),
+        data: data,
+        columns: this._makeInternalId(props.columns),
+        sortField: props.sortField,
+        sortDirection: props.sortDirection,
+        pageSize: props.pageSize,
+        currentPage: props.currentPage
+     }
+
+     if (props.pageSize !== -1 || props.sortField) {
+        if (props.sortField) {
+            data = this.sortBy(props.sortField).data
+        }
+        debugger
+        if(props.pageSize > 0) {
+            data = this.paginate(data, props.pageSize, props.currentPage)
+        }
+
+        this.state.data = data
       }
     }
+
+    paginate (array, page_size, page_number) {
+        --page_number; // because pages logically start with 1, but technically with 0
+        return array.slice(page_number * page_size, (page_number + 1) * page_size);
+      }
 
     _makeInternalId(columns) {
         return columns.map((elem, id) => {return {
@@ -72,23 +92,28 @@ export default class Table extends Component {
 
     _onThClick(evt, columnDef) {
         if(columnDef.sortable) {
-            this.sortBy(columnDef)
+            const data = this.sortBy(columnDef)
+            this.setState({
+                ...data
+            })
         }
     }
 
     sortBy(columnDef) {
+        const fieldName = typeof columnDef === 'string' ? columnDef : columnDef.fieldName
         let {sortField, sortDirection} = this.state
-        if (sortField === columnDef.fieldName) {
+        if (sortField === fieldName) {
             sortDirection = sortDirection === 'asc' ? 'desc' : 'asc'
         } else {
-            sortField = columnDef.fieldName
-            sortDirection = 'desc'
+            sortField = fieldName
+            sortDirection = 'asc'
         }
-        this.setState({
+
+        return {
             data: orderBy(this.state.originalData, [sortField], [sortDirection]),
             sortField,
             sortDirection
-        })
+        }
     }
 
 }
@@ -98,7 +123,9 @@ Table.propTypes = {
     data: PropTypes.array,
     columns: PropTypes.array,
     sortField: PropTypes.string,
-    sortDirection: PropTypes.string
+    sortDirection: PropTypes.string,
+    pageSize: PropTypes.number,
+    curentPage: PropTypes.number
 }
 
 Table.defaultProps = {
@@ -106,5 +133,7 @@ Table.defaultProps = {
     data: [],
     columns: [],
     sortField: null,
-    sortDirection: null
+    sortDirection: null,
+    pageSize: 3,
+    currentPage: 2
   };
